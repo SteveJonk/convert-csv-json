cd0e01218a6d7d3d2f5a03aa0c92dfbf64fb58de
Steve Jonk <stevejonk1993@gmail.com>
2024-08-14 20:19:20 +0200

    feat: add npm stuffz

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const fs = require('fs');
 const csv = require('csv-parser');
 
+const FILE_NAME = 'translations.csv';
+
 function setNestedKey(obj, path, value) {
-  const keys = path.split('.');
-  const lastKey = keys.pop();
-  const lastObj = keys.reduce((obj, key) => (obj[key] = obj[key] || {}), obj);
+  const keys = path?.split('.');
+  const lastKey = keys?.pop();
+  const lastObj = keys?.reduce((obj, key) => (obj[key] = obj[key] || {}), obj);
   lastObj[lastKey] = value;
 }
 
-fs.createReadStream('yourfile.csv')
+fs.createReadStream(FILE_NAME)
   .pipe(csv())
   .on('headers', (headers) => {
     headers.slice(1).forEach((header) => {
