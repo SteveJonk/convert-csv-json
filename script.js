8e83fdbb2c8a255ab9fdbdd403a0947631e433aa
Steve Jonk <stevejonk1993@gmail.com>
2024-08-14 20:15:06 +0200

    feat: add node

---
+++ script.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const csv = require('csv-parser');
+
+function setNestedKey(obj, path, value) {
+  const keys = path.split('.');
+  const lastKey = keys.pop();
+  const lastObj = keys.reduce((obj, key) => (obj[key] = obj[key] || {}), obj);
+  lastObj[lastKey] = value;
+}
+
+fs.createReadStream('yourfile.csv')
+  .pipe(csv())
+  .on('headers', (headers) => {
+    headers.slice(1).forEach((header) => {
+      fs.writeFileSync(`${header}.json`, '{}');
+    });
+  })
+  .on('data', (row) => {
+    Object.keys(row)
+      .slice(1)
+      .forEach((column) => {
+        const data = JSON.parse(fs.readFileSync(`${column}.json`));
+        setNestedKey(data, row['key'], row[column]);
+        fs.writeFileSync(`${column}.json`, JSON.stringify(data, null, 4));
+      });
+  });
--- script.py
+++ script.py
@@ -1,18 +1,33 @@
 import csv
 import json
 
+
+def nested_set(dic, keys, value):
+    for key in keys[:-1]:
+        dic = dic.setdefault(key, {})
+    dic[keys[-1]] = value
+
 filename = 'translations.csv'  # replace with your filename
 
 # Open the CSV
 with open(filename, 'r', encoding='ISO-8859-1') as f:
     reader = csv.reader(f)
-    
+
     # Get the entire data of the file
     rows = list(reader)
-    
+
     # For each column after the first
     for j in range(1, len(rows[0])):
+        # Use the first row of the column as the filename
+        output_filename = f'{rows[0][j]}.json'
+        
+        data = {}
+        
         # Create a new JSON file for this column
-        with open(f'output{j}.json', 'w') as jsonf:
+        with open(output_filename, 'w') as jsonf:
+            for i in range(1, len(rows)):
+                key_parts = rows[i][0].split('.')
+                nested_set(data, key_parts, rows[i][j])
+
             # Write data to this JSON file
-            jsonf.write(json.dumps({rows[i][0]: rows[i][j] for i in range(len(rows))}, indent=4))
+            jsonf.write(json.dumps(data, indent=4))
